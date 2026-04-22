9f1e8162b6fb3ab9b9b39dc12ed04ba43d3ba6fd
Richard Raya <rdxzv.dev@gmail.com>
2026-04-22 17:52:14 -0300

    Revert "completion: Use simple wait queues"
    This reverts commit 9b937834036edcf45992ac1f4e55c239b26b8115.
    
    Change-Id: Ia14da2ba177fc8d37308dd729e081383ad8e2955
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -7985,10 +7985,7 @@ void migrate_disable(void)
 		return;
 	}
 #ifdef CONFIG_SCHED_DEBUG
-	if (unlikely(p->migrate_disable_atomic)) {
-		tracing_off();
-		WARN_ON_ONCE(1);
-	}
+	WARN_ON_ONCE(p->migrate_disable_atomic);
 #endif
 
 	if (p->migrate_disable) {
@@ -8018,10 +8015,7 @@ void migrate_enable(void)
 	}
 
 #ifdef CONFIG_SCHED_DEBUG
-	if (unlikely(p->migrate_disable_atomic)) {
-		tracing_off();
-		WARN_ON_ONCE(1);
-	}
+	WARN_ON_ONCE(p->migrate_disable_atomic);
 #endif
 
 	WARN_ON_ONCE(p->migrate_disable <= 0);
